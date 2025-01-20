67ebc3cdeedfed8b9d38756765083d5bb3c0edde
Yuya Osanai <osanaiyuya1229@gmail.com>
2025-01-20 14:20:50 +0900

    ヘッダー迄完成。

--- frontpage.php
+++
@@ -1,9 +0,0 @@
-<!DOCTYPE html>
-<html <?php language_attributes(); ?>>
-    <head>
-        <meta charset="<?php bloginfo('charset') ?>">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
-        <?php wp_head();?>
-    </head>
