7e39f6ebd508b3b68df3eebd2d7c129f7ec75f3e
Yuya Osanai <osanaiyuya1229@gmail.com>
2025-01-20 16:02:33 +0900

    フロントページに挨拶と商品紹介を追加

--- front-page.php
+++ front-page.php
@@ -49,6 +49,42 @@
                     <div class="home-Hero">
                         <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/Hero-Image.png" alt="">
                     </div>
+                    <div class="top-content">
+                        <div class="main-content-header">
+                            <h1>未来の地球のために、今できることを。</h1>
+                        </div>
+                        <div class="main-content-text">
+                            <p>Green Horizon Inc.は、地球環境に配慮したエコ商品や
+                            <br>サービスを企画・販売する企業です。
+                            <br>持続可能な未来を目指し、サステナブルなライフスタイルを
+                            <br>広める活動を行っています。</p>
+                        </div>
+                        <a href="#" class="main-content-More">詳しくはこちら</a>
+                    </div>
+                </div>
+                <div class="product-Introduction">
+                    <div class="main-content-header">
+                        <h1>製品紹介</h1>
+                    </div>
+                    <div class="image-grid">
+                        <a href="#" class="grid-item">
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/daily_necessities.png" alt="日用品">
+                        </a>
+                        <a href="#" class="grid-item">
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/kitchen_supplies.png" alt="キッチン用品">
+                        </a>
+                        <a href="#" class="grid-item">
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/fashion.png" alt="ファッション">
+                        </a>
+                        <a href="#" class="grid-item">
+                            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/gadget.png" alt="ガジェット">
+                        </a>
+                    </div>
+                </div>
+                <div class="home-news">
+                    <div class="main-content-header">
+                        <h1>新着情報</h1>
+                    </div>
                 </div>
             </main>
 
