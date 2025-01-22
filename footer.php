50be3f397c1428a8d9f0efea7daa7b4be84d2b0b
Yuya Osanai <osanaiyuya1229@gmail.com>
2025-01-22 13:10:59 +0900

    ヘッダーとフッターを分割したよ

---
+++ footer.php
@@ -0,0 +1,26 @@
+
+<footer class="footer">
+                <div class="footer-container row">
+                    <?php if ( is_active_sidebar( 'footer-widget-area' ) ) : ?>
+                        <div class="footer-contents col-md-4">
+                            <?php dynamic_sidebar( 'footer-widget-area' ); ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php if ( is_active_sidebar( 'footer-widget-area-2' ) ) : ?>
+                        <div class="footer-contents col-md-4">
+                            <?php dynamic_sidebar( 'footer-widget-area-2' ); ?>
+                        </div>
+                    <?php endif; ?>
+                    <?php if ( is_active_sidebar( 'footer-widget-area-3' ) ) : ?>
+                        <div class="footer-contents col-md-4">
+                            <?php dynamic_sidebar( 'footer-widget-area-3' ); ?>
+                        </div>
+                    <?php endif; ?>
+                </div>
+                <p class="footer-Copyright">
+                    <small>&copy; 2025 Green Horizon Inc. </small>
+                </p>
+             </footer>
+        </div>
+    </body>
+</html>
---
+++ header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+    <head>
+        <meta charset="<?php bloginfo('charset') ?>">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="<?php bloginfo( 'description' ); ?>">
+        <?php wp_head();?>
+    </head>
+    <body <?php body_class();?>>
+        <?php wp_body_open(  ); ?>
+        <div class="content-Wrap">
+            <header class="header">
+                <div class="header-Icon">
+                    <a class="header-Icon-Link" href="#">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/Logo.png" alt="">
+                    </a>
+                </div>
+                <div class="header-Nav">
+                    <div class="header-Nav-Inner">
+                        <?php wp_nav_menu( 
+                            array(
+                                'theme_location'=>'main-menu',
+                                'menu_class'=>'header-Nav_Items',
+                                'container'=>false,
+                            )
+                        ); ?>
+                    </div>
+                </div>
+                <div class="header-SNS-Icons">
+                    <a href="#" class="header-SNS-Icon-X">
+                        <img class="SNS-Icon" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/x.png" alt="X">
+                    </a>
+                    <a href="#" class="header-SNS-Icon-Instagram">
+                        <img class="SNS-Icon" src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/instagram.png" alt="Instagram">
+                    </a>
+                </div>
+            </header>
+            <?php if(!is_front_page(  )): ?>
+            <?php if ( function_exists( 'bcn_display' ) ) : ?>
+            <nav class="breadCrumb" typeof="BreadcrumbList" vocab="http://schema.org/" aria-label="現在のページ">
+            <?php bcn_display(); ?>
+            </nav>
+            <?php endif; ?>
+            <?php endif; ?>
