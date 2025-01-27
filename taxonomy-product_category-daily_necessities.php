c0ffc7ee0a3f841ac44ba238539aa9aefdb5d17f
Yuya Osanai <osanaiyuya1229@gmail.com>
2025-01-27 16:30:17 +0900

    カスタム投稿アーカイブを少し改修 まだ調整必要

--- taxonomy-product_category-daily_necessities.php
+++ taxonomy-product_category-daily_necessities.php
@@ -1,21 +1,22 @@
 <?php get_header(  ); ?>
 <main class="main">
     <div class="main-Contents-Wrap container-fluid">
-            <div class="home-Hero">
-                <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/dailyHero.png" alt="">
+        <div class="home-Hero">
+            <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/assets/img/dailyHero.png" alt="">
+        </div>
+        <div class="top-content">
+            <div class="main-content-header">
+                <h1>Green Horizon Inc.の日用品</h1>
             </div>
-            <div class="top-content">
-                <div class="main-content-header">
-                    <h1>Green Horizon Inc.の日用品</h1>
-                </div>
-                <div class="main-content-text">
-                    <p>Green Horizon Inc.の日用品シリーズは
-                    <br>日常生活の中で環境に配慮した選択をサポートするためにデザインされています。
-                    <br>再生可能な素材や自然由来の成分を活用し
-                    <br>使い捨てを減らす持続可能な商品を提供します
-                    <br>機能性とエコロジーを両立させることで、地球に優しいライフスタイルを提案します。</p>
-                </div>
+            <div class="main-content-text">
+                <p>Green Horizon Inc.の日用品シリーズは
+                <br>日常生活の中で環境に配慮した選択をサポートするためにデザインされています。
+                <br>再生可能な素材や自然由来の成分を活用し
+                <br>使い捨てを減らす持続可能な商品を提供します
+                <br>機能性とエコロジーを両立させることで、地球に優しいライフスタイルを提案します。</p>
             </div>
+        </div>
+        <div class="products-List-Container">
             <div class="products-List-Wrap row">
                 <?php if(have_posts(  )): ?>
                     <?php 
@@ -26,6 +27,7 @@
                     <?php endwhile; ?>
                 <?php endif; ?>
             </div>
+        </div>
     </div>
 </main>
 
